a2ac22b9cf9fcf4a86c646953dbb62c400825f6c
kokoryh <kokoryh@qq.com>
2025-02-24 21:50:15 +0800

    perf(missav): compress html elements

--- src/script/missav/missav.html.js
+++ src/script/missav/missav.html.js
@@ -14,50 +14,8 @@ const headers = $response.headers;
 const contentType = headers['content-type'];
 if (!contentType?.includes('text/html')) $done({});
 
-const scriptElement = `
-<script>
-    (function () {
-        'use strict';
-        document.addEventListener('ready', () => {
-            window.open = () => {};
-            if (window.player?.pause) {
-                const pause = window.player.pause;
-                window.player.pause = () => {
-                    if (document.hasFocus()) {
-                        pause();
-                    }
-                };
-            }
-        });
-    })();
-</script>
-`;
-const styleElement = `
-<style>
-    .lg\\:hidden {
-        display: none !important;
-    }
-    .lg\\:block {
-        display: none !important;
-    }
-    a[href*="//bit.ly/"]
-    {
-        display: none !important;
-    }
-    .relative > div[x-init*='campaignId=under_player'] {
-        display: none !important;
-    }
-    div[style*='width: 300px; height: 250px;'] {
-        display: none !important;
-    }
-    div:has(a[href*='go.myavlive.com']) {
-        display: none !important;
-    }
-    div[x-show^='recommendItems'] ~ div[class]:has(> div > div.mx-auto > div.flex > a[rel^='sponsored']) {
-        display: none !important;
-    }
-</style>
-`;
+const scriptElement = `<script>(function(){'use strict';document.addEventListener('ready',()=>{window.open=()=>{};if(window.player?.pause){const pause=window.player.pause;window.player.pause=()=>{if(document.hasFocus()){pause()}}}})})();</script>`;
+const styleElement = `<style>.lg\\:block,.lg\\:hidden,a[href*="//bit.ly/"],div:has(a[href*='go.myavlive.com']),div[style*='width: 300px; height: 250px;'],.relative>div[x-init*='campaignId=under_player'],div[x-show^='recommendItems']~div[class]:has(>div>div.mx-auto>div.flex>a[rel^='sponsored']){display:none!important}</style>`;
 const scriptElementFilter = (i, element) => {
     if (element.attribs?.src?.includes('cdn.tsyndicate.com')) {
         return true;
